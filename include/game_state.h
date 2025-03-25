6c52418ad32eda73e491ff28a32e5da5370f2533
Tariq Zaghal <zaghaltariq007@gmail.com>
2025-03-25 22:45:35 +0200

    modify fields of game_state declaration

--- include/game_state.h
+++ include/game_state.h
@@ -9,16 +9,20 @@
 
 typedef struct GameState {
   char in_round;
-  time_t round_start_time;
+  //time_t round_start_time;
+  time_t simulation_time;
+  time_t round_period;
+  int number_of_rounds;
   int round_score;
   PairIntInt simulation_score;
-  time_t simulation_start_time;
+  //time_t simulation_start_time; 
   Team team1;
   Team team2;
 } GameState;
 
 int init_game_state(GameState *game_state, GameConfig *game_config,
-                    TeamConfig *team1_config, TeamConfig *team2_config);
+                    //TeamConfig *team1_config, TeamConfig *team2_config
+                    Team *team1, Team *team2);
 int serialize_game_state(GameState *game_state, StringBuf *string_buf);
 int deserialize_game_state(GameState *game_state, StringBuf *string_buf);
 #endif
